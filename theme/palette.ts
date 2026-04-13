2bc82e816273f997d8e8934b72e4503ca68726f6
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2026-04-13 10:01:51 +0100

    feat(theme): add color palette

---
+++ theme/palette.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  background: "#F9F7F7", // Fondo general de la app
+  dark: "#112D4E", // Azul oscuro
+  primary: "#3F72AF", // Azul primario
+  secondary: "#C8D5EC", // Azul secondario
+  black: "#1F1F1F", // Negro por defecto
+  white: "#FFFFFF", // Blanco por defecto
+  shadow: "#000000", // Color de la sombra
+};
